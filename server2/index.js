0007e1145dade84019010885ec62051dfc8f710f
Vaishnavi T.G <vaishnavitg2@gmail.com>
2025-03-13 20:08:03 +0530

    Added all files

---
+++ server2/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser=require("body-parser")
+const port = 3000
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/', function (req, res)  {
+  res.send('Hello World!')
+})
+app.get('/route-handler', function (req, res)  {
+  res.send('Hello World 2!')
+})
+app.listen(port)
+
+
+
+
